3e6af59182b87f91a1bde80182fe81d11356c480
Alyona Goncharova <bestliona@gmail.com>
2017-04-21 17:05:02 +0300

    Add memory usage

--- generate.js
+++ generate.js
@@ -1,33 +1,41 @@
 "use strict";
 const fs = require('fs');
 
-function generateCVS(fileName, headers, amount){
+const getMemory = setInterval(()=>{
+    console.log((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2), 'mb');
+}, 30);
 
-    fs.open(fileName, "w+" , function(err, file_handle){
+function generateCSV(fileName, headers, amount) {
+
+    fs.open(fileName, "w+", function (err, file_handle) {
+
         let text = '';
 
-        for (let k = 0; k < headers.length ; k++){
-            if(headers[k].indexOf(',')>0)
-                text += '"'+ headers[k] + '"';
+        //write headers
+        headers.forEach((item, i, arr) => {
+            if (i == (arr.length - 1))
+                text += item;
             else
-                text += headers[k] + ',';
-        }
+                text += item + ',';
+        });
 
         text += '\n';
 
-        for(let i =0; i< amount; i++){
-            for(let j=0; j< headers.length; j++){
-                text += headers[j] + '_'+i;
-                if(j != headers.length-1 )
+        //generate content
+        for (let i = 0; i < amount; i++) {
+            for (let j = 0; j < headers.length; j++) {
+                text += headers[j] + '_' + i;
+                if (j != headers.length - 1)
                     text += ',';
             }
             text += '\n';
         }
+
         fs.write(file_handle, text);
+        fs.close(file_handle);
     });
-
-
-
 }
 
-generateCVS('test.txt',['id','title'], 100000);
+generateCSV('book.csv', ['id', 'title'], 1000000);
+
+//clearInterval(getMemory);
