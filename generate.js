99f2f2739e9da81ae8a420aa3a69374d12f6f3da
Alyona Goncharova <bestliona@gmail.com>
2017-04-24 16:27:06 +0300

    Add promise 'write line'

--- generate.js
+++ generate.js
@@ -7,45 +7,42 @@ const getMemory = setInterval(()=>{
 getMemory.unref();
 
 function generateCSV(fileName, headers, amount) {
-
-
-
     return new Promise((resolve, reject) => {
         const file = fs.createWriteStream(fileName);
         file.on('error', (er)=> reject(er));
-        file.on('close', (er)=> resolve('done'));
 
         //write headers
         file.write(headers.map(field => '"' + field + '"').join(',') + '\n');
 
         let i = 0;
-        write();
+        writeFile();
 
-        //generate content
-        function write() {
-            let ok = true;
-            do {
-                i++;
-                //generate line
-                let text = headers.map(field => '"' + field + '_' + i + '"').join(',') + '\n';
+        function writeFile() {
+            let text = headers.map(field => '"' + field + '_' + i + '"').join(',') + '\n';
 
-                //if last line => callback
-                if (!file.write(text)) {
-                    file.once('drain', write);
-                    ok = false;
-                }
-                if (i == amount)
-                    file.close();
-            } while (i < amount && ok);
+            writeLine(file, text)
+                .then( () => {
+                    if(i == amount)
+                        resolve('Done');
+                    else {
+                        i++;
+                        writeFile();
+                    }}
+                )
+                .catch(()=> { file.once('drain', writeFile );  });
         }
-
-
+    });
+}
+function writeLine(file, text) {
+    return new Promise((resolve, reject)=>{
+        if(!file.write(text)) {
+            reject();
+        }
+        else
+            resolve();
     });
 }
 
 generateCSV('book.csv', ['id', 'title'], 1000000)
-    .then(
-        done => {console.log('done')}
-    )
+    .then( done => {console.log('done')})
     .catch( er => {console.log(er)});
-
